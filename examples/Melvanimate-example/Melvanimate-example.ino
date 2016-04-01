e00d607c6c09262f317552237d9a9e7be6c4eaa9
Andrew Melvin <amelvin@Andrew-MBA.local>
2016-04-01 21:36:39 +0100

    Fixes to main ino.. fix baud rate bug

--- examples/Melvanimate-example/Melvanimate-example.ino
+++ examples/Melvanimate-example/Melvanimate-example.ino
@@ -69,10 +69,10 @@
 using namespace helperfunc;
 
 
-extern "C"  {
-  #include <cont.h>
-  extern cont_t g_cont;
-}
+// extern "C"  {
+//   #include <cont.h>
+//   extern cont_t g_cont;
+// }
 
 //  this is the native SDK json lib.
 //#include <json/json.h>
@@ -80,7 +80,9 @@ extern "C"  {
 
 ESP8266WebServer HTTP(80);
 FSBrowser fsbrowser(HTTP);
-IPAddress mqtt_server_ip(192, 168, 1, 1);
+//IPAddress mqtt_server_ip(192, 168, 1, 1);
+
+
 SimpleTimer timer;
 
 //ESPmanager settings(HTTP, SPIFFS, "Melvanimate-square", "SKY", "wellcometrust");
@@ -109,7 +111,7 @@ void install_effects()
   lights.Add("Blobs", new Blobs); 
 
   lights.Add("CuriousCat",   new Effect2);
-  lights.Add("Adalight",     new AdalightEffect(Serial, 115000));    // working - need to test
+  lights.Add("Adalight",     new AdalightEffect(Serial, 115200));    // working 
   lights.Add("UDP",          new UDPEffect);                        // working
   lights.Add("DMX",          new DMXEffect );                       // need to test - requires custom libs included
   
@@ -185,15 +187,15 @@ void setup()
 
   HTTP.on("/restart", HTTP_ANY, []() {         ESP.restart();   });
 
-  HTTP.on("/memory", HTTP_ANY, []() {
-     size_t freemem = cont_get_free_stack(&g_cont);
+  // HTTP.on("/memory", HTTP_ANY, []() {
+  //    size_t freemem = cont_get_free_stack(&g_cont);
 
-    Serial.printf("[stack] free = %u\n",freemem ); 
-    Serial.printf("[heap] free = %u\n",ESP.getFreeHeap() ); 
-    HTTP.send(200); // sends OK if were just receiving data...
+  //   Serial.printf("[stack] free = %u\n",freemem ); 
+  //   Serial.printf("[heap] free = %u\n",ESP.getFreeHeap() ); 
+  //   HTTP.send(200); // sends OK if were just receiving data...
 
 
-  });
+  // });
 
   HTTP.on("/debug", HTTP_GET, []() {
     static bool debugstate = false;
@@ -252,20 +254,6 @@ void setup()
     HTTP.send(200); // sends OK if were just receiving data...
   }
 
-  // if (HTTP.hasArg("list")) {
-
-  //   Serial.printf("[list] _numberofpresets = %u\n", lights._numberofpresets);
-
-  //   for (uint8_t i = 0; i < lights._numberofpresets; i++) {
-
-  //     char * text = lights._preset_names[i];
-
-
-  //     Serial.printf("[%u] %u (%s)\n", i, lights._presets[i], text) ;
-
-  //   }
-  // }
-
   });
 
   //void serveStatic(const char* uri, fs::FS & fs, const char* path, const char* cache_header = NULL );
@@ -277,166 +265,29 @@ void setup()
 
 
 
-
-  //timer.setTimeout(5000, []() { lights.Start("Marquee");} ) ;
-
-  // timer.setInterval(1000, []() {
-  //   Debugf("HEAP: %u\n", ESP.getFreeHeap());
-  // });
-
-  //timer.setTimeout(1000, []() { lights.Start("BobblySquares");} ) ;
-
-
-  // timer.setTimeout(1000, []() {
-  //   Serial.println("\n");
-  //   Serial.println("------START-----");
-  //   lights.Start("SimpleColor");
-  //   Serial.println("-------END------");
-
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(255, 0, 0));
-
-  //   lights.Save(1, "RED", true);
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(0, 0, 255));
-
-  //   Serial.println();
-  //   lights.Save(2, "GREEN", true);
-
-  //   Serial.println();
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(0, 255, 0));
-  //   lights.Save(3, "BLUE", true);
-  //   Serial.println();
-
-  // }) ;
-
-  // timer.setTimeout(1500, []() {
-  //   Serial.println("\n");
-  //   Serial.println("------START-----");
-  //   lights.Start("CuriousCat");
-  //   Serial.println("-------END------");
-
-  //   lights.Current()->setVar<uint8_t>("int2", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int3", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int4", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int5", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int6", random(0, 10000));
-
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(255, random(0, 255), random(0, 255)));
-  //   lights.Current()->setVar<RgbColor>("color3", RgbColor(255, random(0, 255), random(0, 255)));
-
-
-  //   lights.Save(1, "CuriousCatRED", true);
-  //   lights.Current()->setVar<uint8_t>("int2", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int3", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int4", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int5", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int6", random(0, 10000));
-
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(0, random(0, 255), 255));
-  //   lights.Current()->setVar<RgbColor>("color3", RgbColor(255, random(0, 255), random(0, 255)));
-
-  //   Serial.println();
-  //   lights.Save(2, "CuriousCatGREEN", true);
-
-  //   Serial.println();
-  //   lights.Current()->setVar<uint8_t>("int2", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int3", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int4", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int5", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("int6", random(0, 10000));
-  //   lights.Current()->setVar<uint8_t>("brightness", random(0, 255));
-  //   lights.Current()->setVar<RgbColor>("color1", RgbColor(random(0, 255), 255, 0));
-  //   lights.Current()->setVar<RgbColor>("color3", RgbColor(255, random(0, 255), random(0, 255)));
-
-  //   lights.Save(3, "CuriousCatBLUE", true);
-  //   Serial.println();
-
-  //   lights.Start("Off");
-
-  // }) ;
-
-
-
-  // timer.setTimeout(2000, []() {
-
-  //   DynamicJsonBuffer jsonBuffer;
-  //   JsonObject & root = jsonBuffer.createObject();
-  //   lights.addAllpresets(jsonBuffer, root);
-
-  // }) ;
-
-  // timer.setTimeout(2100, []() {
-
-  //   Serial.println("\n");
-  //   Serial.println("------START-----");
-  //   lights.Load("2.4");
-  //   Serial.println("-------END------");
-
-  // }) ;
-
-
-  // timer.setInterval(5000, []() {
-  //   uint8_t effect = random(2, 4);
-  //   uint8_t id = random(1, 4);
-
-  //   Serial.println("------START-----");
-
-  //   lights.Load(effect, id);
-  //   Serial.println("-------END------\n");
-
-  // });
-
-
-
   lights.Start("Off");
 
-  // timer.setInterval(500, []() {
-  //   text2Fn("Wellcome To the Jungle");
-  // });
 
   Debugf("HEAP: ");
   Debugf("%u\n", ESP.getFreeHeap());
 
   Debugf("Melvanimate Ready\n");
 
-  // lights.Current()->GeneralEffect::*pmf()
-
-  //bool (GeneralEffect::*fptr) (uint8_t) = &GeneralEffect::setBrightness;
 
 
-//  (*fptr)(100);
-
-  // Melvtrix & matrix =  *lights.matrix();
-
-  // ShapeUpdateCallback ShapeCallback = [](uint16_t pixel, int16_t x, int16_t y) {
-  //   Serial.printf("%3u ", pixel);
-  // };
+  //network.send(44, "ESPtest123", 10, 1000000);
+  network.set_receiver(receiver_function);
 
 
-  // matrix.setShapeFn( ShapeCallback );
+  timer.setInterval(1000, []() {
+    uint32_t tick = micros();
+    size_t len = strlen(settings.getHostname());
 
+    network.send(44, (char*)settings.getHostname(), len);
 
-  // for (int x = 0; x < matrix.width(); x++) {
-  //   for (int y = 0; y < matrix.height(); y++) {
-  //     matrix.drawPixel(x, y);
-  //   }
-  //   Serial.println();
-  // }
-
-  // lights.palette().getModeString();
-
-
-  // do {
-  //   Serial.println("remove presets");
-  // } while (SPIFFS.remove(PRESETS_FILE) );
-
-  // do {
-  //   Serial.println("remove presets");
-  // } while (SPIFFS.remove("/MelvanaSettings.txt") );
+   
+    //Serial.printf("[PIRC send] %uus\n", micros() - tick); 
+  });
 
 }
 
