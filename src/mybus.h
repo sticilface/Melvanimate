448df5c6714508e96d53cf8609730a784e80976f
Andrew Melvin <amelvin@Andrew-MBA.local>
2016-04-06 10:32:09 +0100

    Some example additions to mybus.h

--- src/mybus.h
+++ src/mybus.h
@@ -2,4 +2,29 @@
 
 #include "NeopixelBus.h"
 
+//  
+/* Features
+
+	NeoGrbFeature
+	NeoRgbFeature
+	NeoBrgFeature
+
+
+*/
+	
+/* Methods
+
+	NeoEsp8266Uart800KbpsMethod
+	
+	
+
+
+*/
+
+
+
+// GRB LEDs , UART method
 typedef NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> MyPixelBus;
+
+
+//typedef NeoPixelBus< NeoBrgFeature, NeoEsp8266Uart800KbpsMethod> MyPixelBus;
