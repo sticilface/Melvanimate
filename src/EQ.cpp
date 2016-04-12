cf511b9f4672f3185cd525456988a8125e5c20d4
Andrew Melvin <amelvin@Andrew-MBA.local>
2016-04-12 23:30:44 +0100

    Fist draft of EQ class, for MSGEQ7

---
+++ src/EQ.cpp
@@ -0,0 +1,200 @@
+#include "EQ.h"
+
+#define EQFILTER 80
+
+EQ::~EQ()
+{
+	_deinitialise();
+};
+
+
+void EQ::StartEQ()
+{
+	pinMode(_strobePin, OUTPUT);
+	pinMode(_resetPin, OUTPUT);
+	digitalWrite(_resetPin, LOW);
+	digitalWrite(_strobePin, HIGH);
+}
+
+void EQ::GetEQ(uint16_t * data)
+{
+	digitalWrite(_resetPin, HIGH);
+	digitalWrite(_resetPin, LOW);
+	digitalWrite(_strobePin, LOW);
+
+	for (int i = 0; i < 7; i++) {
+		data[i] = analogRead(0);  // the time taken is for the analogue read....
+		digitalWrite(_strobePin, HIGH);
+		digitalWrite(_strobePin, LOW);
+	}
+}
+
+void EQ::EndEQ()
+{
+	pinMode(_strobePin, INPUT);
+	pinMode(_resetPin, INPUT);
+}
+
+void EQ::loop()
+{
+
+	if (_enabled && millis() - _tick > _freq) {
+
+		//uint16_t data[7];
+		GetEQ(data);
+
+
+		for (uint8_t i = 0; i < 7; i++) {
+			data[i] = constrain(data[i], EQFILTER, 1023);
+			uint8_t value = map(data[i], EQFILTER, 1023, 0, 255);
+
+			if (_data[i]) {
+
+				EQData_s & current = *_data[i];
+
+				if (value > 100 &&
+				        value > (current.average() * _peakfactor) &&
+				        millis() - current.lastbeat > _beatskiptime
+				   ) {
+					EQParam params;
+					params.channel = i;
+					params.average = current.average();
+					params.level = value;
+					params.bpm = ( 60000 / millis() - current.lastbeat );
+					EQcallbackFN(params);
+					current.lastbeat = millis();
+				}
+
+				current.add(value);
+			}
+		}
+		_tick = millis();
+	}
+
+}
+
+void EQ::_deinitialise()
+{
+	DebugEQf("[EQ::_deinitialise] Called\n");
+	if (_data) {
+		for (uint8_t i = 0; i < 7; i++) {
+			if (_data[i]) {
+				delete _data[i];
+				_data[i] = nullptr;
+			}
+		}
+		delete[] _data;
+		_data = nullptr;
+	}
+
+	EndEQ();
+}
+
+void EQ::Initialise(uint32_t samples, uint32_t totaltime)
+{
+	_samples = samples;
+	_sampletime = totaltime;
+	_freq = totaltime / samples ;
+	DebugEQf("[EQ::initialise] Called %u, %ums, freq = %u\n", samples, totaltime,_freq);
+
+	_deinitialise();
+	uint32_t startheap = ESP.getFreeHeap() ; 
+	_data = new EQData_s*[7];
+
+	for (uint8_t i = 0; i < 7; i++) {
+		_data[i] = new EQData_s(samples);
+	}
+
+	DebugEQf("[EQ::initialise] END heap used = %u\n", startheap - ESP.getFreeHeap() );
+
+	StartEQ();
+
+}
+
+bool EQ::addJson(JsonObject & root)
+{
+
+	JsonObject& EQjson = root.createNestedObject("EQ");
+	EQjson["enablebeats"] = _enabled;
+	EQjson["resetpin"] = _resetPin;
+	EQjson["strobepin"] = _strobePin;
+	EQjson["peakfactor"] = _peakfactor;
+	EQjson["beatskiptime"] = _beatskiptime;
+
+	EQjson["samples"] = _samples;
+	EQjson["sampletime"] = _sampletime;
+
+
+
+}
+
+bool EQ::parseJson(JsonObject & root)
+{
+	bool changed = false;
+
+	if (!root.containsKey("EQ") ) {
+		return false;
+	}
+
+	JsonObject& EQjson = root["EQ"];
+
+	if (EQjson.containsKey("enablebeats")) {
+		if (_enabled != EQjson["enablebeats"]) {
+			_enabled = EQjson["enablebeats"];
+			changed = true;
+		}
+	}
+
+	if (EQjson.containsKey("resetpin")) {
+		if (_resetPin != EQjson["resetpin"]) {
+			_resetPin = EQjson["resetpin"];
+			changed = true;
+		}
+	}
+
+	if (EQjson.containsKey("strobepin")) {
+		if (_strobePin != EQjson["strobepin"]) {
+			_strobePin = EQjson["strobepin"];
+			changed = true;
+		}
+	}
+
+	if (EQjson.containsKey("peakfactor")) {
+		if (_peakfactor != EQjson["peakfactor"]) {
+			_peakfactor = EQjson["peakfactor"];
+			changed = true;
+		}
+	}
+
+	if (EQjson.containsKey("beatskiptime")) {
+		if (_beatskiptime != EQjson["beatskiptime"]) {
+			_beatskiptime = EQjson["beatskiptime"];
+			changed = true;
+		}
+	}
+
+	if (EQjson.containsKey("samples") &&  EQjson.containsKey("sampletime")  ) {
+		if (_samples != EQjson["samples"] ||  _sampletime != EQjson["sampletime"]) {
+			
+			_samples = EQjson["samples"];
+			_sampletime = EQjson["sampletime"];
+			//Initialise(_samples, _sampletime);
+			changed = true;
+		}
+	}
+
+	if (changed && _enabled) {
+		Initialise(_samples, _sampletime);
+	} else {
+		_deinitialise();
+	}
+
+#ifdef DebugEQ
+	DebugEQf("EQ Json\n");
+	EQjson.prettyPrintTo(Serial);
+	DebugEQf("\n");
+#endif
+
+
+}
+
---
+++ src/EQ.h
@@ -0,0 +1,195 @@
+/*
+
+Provides Methods for MSGEQ7 chip
+
+Checked with OsillyScope and the delays are not required.
+MSGEQ7 chip RESET pulse is 0.1us min, strobe pulse 0.018us min.  And it works fine without any delays.
+
+
+
+*/
+
+
+#pragma once
+#include <arduino.h>
+#include <functional>
+#include "PropertyManager.h"
+#include <ArduinoJson.h>
+
+#define DebugEQ
+
+#ifdef DebugEQ
+#define DebugEQf(...) Serial.printf(__VA_ARGS__)
+#else
+#define DebugEQf(...) {}
+#endif
+
+struct EQParam {
+	uint8_t channel;
+	uint16_t level;
+	uint8_t bpm;
+	uint8_t average;
+};
+
+typedef std::function<void(const EQParam& param)> EQCallback;
+
+struct EQData_s {
+	EQData_s(uint16_t size)
+	{
+		_size = size;
+		_data = new uint8_t[size];
+	}
+	~EQData_s()
+	{
+		if (_data) { delete[] _data; }
+	}
+
+	void add(uint8_t value)
+	{
+		if (_data) {
+			_sum -= _data[_position];
+			_data[_position] = value;
+			_sum += _data[_position];
+			_position++;
+			if (_position == _size) { _position = 0; }
+			if (_count < _size) { _count++; }
+		}
+	};
+
+	uint8_t average()
+	{
+		if (_count) {
+			return _sum / _count;
+		}
+		return 0;
+	}
+
+	// uint8_t max()
+	// {
+	// 	uint8_t value = 0;
+	// 	for (uint32_t i = 0; i < _count; i++ ) {
+	// 		if (_data[i] > value) {
+	// 			value = _data[i];
+	// 		}
+	// 	}
+	// 	return value;
+	// }
+
+	// uint8_t min()
+	// {
+	// 	uint8_t value = _data[0];
+
+	// 	for (uint32_t i = 0; i < _count; i++ ) {
+	// 		if (_data[i] < value) {
+	// 			value = _data[i];
+	// 		}
+	// 	}
+	// 	return value;
+	// }
+
+	uint32_t lastbeat{0};
+
+private:
+	uint32_t _size{0};
+	uint32_t _count{0};
+	uint32_t _position{0};
+	//uint8_t _channel{0};
+	uint32_t _sum{0};
+	uint8_t * _data{nullptr};
+
+
+
+};
+
+
+class EQ
+{
+public:
+	EQ() {};
+	~EQ();
+
+	//void DetectBeats(bool value) { _detectbeats = value;}
+	void Initialise(uint32_t samples, uint32_t totaltime);
+	void SetBeatCallback(EQCallback EQcallback) {EQcallbackFN = EQcallback;};
+
+	void loop();
+
+	void Start() { _enabled = true; }
+	void Stop() { _enabled = false; }
+	void End() { Stop(); _deinitialise(); }
+	
+	uint16_t data[7] = {0};
+
+
+	void GetEQ(uint16_t * data);
+	void EndEQ();
+	void StartEQ();
+
+	bool addJson(JsonObject & root);
+	bool parseJson(JsonObject & root); 
+
+private:
+	void _deinitialise(); 
+
+	bool _enabled {false}; 
+
+	uint8_t _resetPin = 12;
+	uint8_t _strobePin = 13;
+
+	uint32_t _tick{0};
+
+	EQData_s ** _data{nullptr};
+
+	uint32_t _samples = 0;
+	uint32_t _sampletime = 0; 
+	uint32_t _freq = 0;
+	EQCallback EQcallbackFN;
+
+	float _peakfactor{2.1};
+	uint8_t _beatskiptime{200};
+
+};
+
+
+
+template <>
+class Variable<EQ*>: public AbstractPropertyHandler
+{
+public:
+	Variable()
+	{
+		_name = "EQ";
+		_var = new EQ;
+	};
+	~Variable() override
+	{
+		if (_var) {
+			delete _var;
+			_var = nullptr;
+		}
+	}
+
+	EQ * get() { return _var; }
+
+	void set(EQ * value) { _var = value; }
+
+	bool addJsonProperty(JsonObject & root, bool onlychanged = false) override
+	{
+		if (onlychanged && !_changed) { return false; }
+		return _var->addJson(root);
+	}
+
+	bool parseJsonProperty(JsonObject & root) override
+	{
+		if (root.containsKey(_name)) {
+			_changed = _var->parseJson(root);
+			return _changed;
+		} else {
+			return false;
+		}
+	}
+
+private:
+	EQ * _var{nullptr};
+};
+
