d6135b91f88973750d29ad85620fb028fd2dffd8
sticilface <amelvin@gmail.com>
2016-06-14 19:18:39 +0100

    add travis:)

---
+++ travis/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+
+echo "script started"
+
+# generate the manifest
+python $TRAVIS_BUILD_DIR/scripts/buildmanifest.py /tmp/package /tmp/package/manifest.json
+
+#  no host checking
+#ssh -v -p 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key $HOME_USER@$HOME_IP "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"
+#scp -v -P 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"
+#echo "repo slug = $TRAVIS_REPO_SLUG"
+
+echo "DEPLOY HOME"
+
+ssh -v -p 4022  -i /tmp/travis.key $HOME_USER@www.amelvin.co.uk "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"
+scp -v -P 4022  -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@www.amelvin.co.uk:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"
